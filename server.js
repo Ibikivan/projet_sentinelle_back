ea5df178f7e8866904df39a39a2202b5cd111d12
Roche Ngaleu <ibikivan1@gmail.com>
2025-03-31 01:03:40 +0200

    Nouveau fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur mise à jour 2 !');
+    res.end('Voilà la réponse du serveur mise à jour 3 !');
 });
 
 server.listen(process.env.PORT || 3000);
