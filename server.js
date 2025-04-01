76688a7367ef4f587c3801287c0ad955c21304cf
Roche Ngaleu <ibikivan1@gmail.com>
2025-04-01 22:10:05 +0200

    Encore un fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur mise à jour 8 !');
+    res.end('Voilà la réponse du serveur mise à jour 9 !');
 });
 
 server.listen(process.env.PORT || 3000);
