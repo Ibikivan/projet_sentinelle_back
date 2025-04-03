5d182ef4cdae177bb6575bfa1f3a4eca5d9704ba
Roche Ngaleu <ibikivan1@gmail.com>
2025-04-04 01:47:20 +0200

    Nouveau déploiement

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur mise à jour 11 !');
+    res.end('Voilà la réponse du serveur mise à jour 13 !');
 });
 
 server.listen(process.env.PORT || 3000);
