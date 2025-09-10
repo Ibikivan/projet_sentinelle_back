d22b4eec87747a1f0c1efbde0662bb3ff7ecabd8
Ibikivan <82176594+Ibikivan@users.noreply.github.com>
2025-09-11 00:20:20 +0200

    FIX - Environments variables unification

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,21 @@
 module.exports = {
   apps: [{
     name: "sentinelle_api",
-    script: "server.js",  // Adaptez à votre point d'entrée
+    script: "server.js",
     exec_mode: "fork",
     autorestart: true,
     env: {
-      NODE_ENV: "developpement",
+      NODE_ENV: "development",
       PORT: 3000,
       // DB_HOST: "${{ secrets.DB_HOST }}",  // À remplacer par vos variables
+    },
+    env_test: {
+      NODE_ENV: "test",
+      PORT: 3000,
+    },
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 3000,
     },
   }]
 };
